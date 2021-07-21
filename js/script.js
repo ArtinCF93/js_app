bacd65c7d42c5898741045699499a1f4852eab05
ArtinCF93 <Toojool@gmail.com>
2021-07-21 14:13:07 -0700

    Correction of type to type(s)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-    {name: 'Blastoise', type: 'water', weight: 85},
-    {name: 'Charizard', type: 'fire', weight: 90},
-    {name: 'Venasaur', type: 'grass', weight: 100},
+    {name: 'Blastoise', types: 'water', weight: 85},
+    {name: 'Charizard', types: 'fire', weight: 90},
+    {name: 'Venasaur', types: 'grass', weight: 100},
 ];
