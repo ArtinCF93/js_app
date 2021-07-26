d88c16321e887a6851f2aa4b4675cb985698ab36
ArtinCF93 <Toojool@gmail.com>
2021-07-26 15:29:24 -0700

    For loop
    For loop, created for ex.3

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 let pokemonList = [
     {name: 'Blastoise', types: 'water', weight: 85},
-    {name: 'Charizard', types: 'fire', weight: 90},
-    {name: 'Venasaur', types: 'grass', weight: 100},
+    {name: 'Charizard', types: ['fire','flying'], weight: 90},
+    {name: 'Venasaur', types: ['grass','poison'], weight: 100},
 ];
+for (let i=0; i<pokemonList.length; i++){
+    if (pokemonList[i].weight <= 89){
+        document.write(pokemonList[i].name + '(Weight:' + pokemonList[i].weight + ')' );document.write("<br>");}
+    else if (pokemonList[i].weight <= 95){
+        document.write(pokemonList[i].name + '(Weight:' + pokemonList[i].weight + ')');document.write("<br>");}
+    else if 
+    (pokemonList[i].weight <=100){
+        document.write(pokemonList[i].name + '(Weight:' + pokemonList[i].weight + ') - Wow! that is big');}
+    }
+    
