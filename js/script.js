57f4d0eb15ff15f117969bf72497ba16c35862ef
ArtinCF93 <Toojool@gmail.com>
2021-07-29 20:44:26 -0700

    Adding functions to Pokemon List

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
-let pokemonList = [
-    {name: 'Blastoise', types: 'water', weight: 85},
-    {name: 'Charizard', types: ['fire','flying'], weight: 90},
-    {name: 'Venasaur', types: ['grass','poison'], weight: 100},
-];
-for (let i=0; i<pokemonList.length; i++){
-    if (pokemonList[i].weight <= 89){
-        document.write(pokemonList[i].name + '(Weight:' + pokemonList[i].weight + ')' );document.write("<br>");}
-    else if (pokemonList[i].weight <= 95){
-        document.write(pokemonList[i].name + '(Weight:' + pokemonList[i].weight + ')');document.write("<br>");}
-    else if 
-    (pokemonList[i].weight <=100){
-        document.write(pokemonList[i].name + '(Weight:' + pokemonList[i].weight + ') - Wow! that is big');}
+let pokemonRepository = (function() {
+    let pokemonList = [
+        {name: 'Blastoise', types: 'water', weight: 85},
+        {name: 'Charizard', types: ['fire','flying'], weight: 90},
+        {name: 'Venasaur', types: ['grass','poison'], weight: 100},
+    ];
+
+pokemonList.forEach(function(pokemon){
+        document.write(pokemon.name + '(Weight:' + pokemon.weight + ')' )
+        document.write("<br>");}
+)
+
+    return {
+        add: function(pokemon1){
+            pokemonList.push(pokemon1);
+        },
+        getAll: function(){
+            return pokemonList;
     }
-    
+};
+
+})();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({name:'Mewtwo', types: 'unkown', weight: 40});
+console.log(pokemonRepository.getAll());
