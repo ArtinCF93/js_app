f7a9a551ccffde0fae9ce9a5e7fd20e767fdf314
ArtinCF93 <Toojool@gmail.com>
2021-07-31 14:30:12 -0700

    correction of conditional Ex. 1.5

--- js/script.js
+++ js/script.js
@@ -1,26 +1,33 @@
-let pokemonRepository = (function() {
+let pokemonRepository = (function () {
     let pokemonList = [
-        {name: 'Blastoise', types: 'water', weight: 85},
-        {name: 'Charizard', types: ['fire','flying'], weight: 90},
-        {name: 'Venasaur', types: ['grass','poison'], weight: 100},
+        { name: 'Blastoise', types: ['water'], weight: 85 },
+        { name: 'Charizard', types: ['fire', 'flying'], weight: 90 },
+        { name: 'Venasaur', types: ['grass', 'poison'], weight: 100 },
     ];
 
-pokemonList.forEach(function(pokemon){
-        document.write(pokemon.name + '(Weight:' + pokemon.weight + ')' )
-        document.write("<br>");}
-)
 
     return {
-        add: function(pokemon1){
-            pokemonList.push(pokemon1);
+        add: function (pokemon1) {
+            if (pokemon1.name && pokemon1.types && pokemon1.weight){
+                pokemonList.push(pokemon1);
+            } else {
+                console.log('Not a Pokemon')
+            }
         },
-        getAll: function(){
+        getAll: function () {
             return pokemonList;
+        }
     }
-};
+}
+)();
 
-})();
+pokemonRepository.add({ name: 'Mewtwo', types: ['unkown'], weight: 0 });
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+    document.write(pokemon.name + '(Weight:' + pokemon.weight + ')')
+    document.write("<br>");
+}
+)
 
 console.log(pokemonRepository.getAll());
-pokemonRepository.add({name:'Mewtwo', types: 'unkown', weight: 40});
 console.log(pokemonRepository.getAll());
