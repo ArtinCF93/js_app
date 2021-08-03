043da68de705f582140c32ca1ccf29a29bc0ef28
ArtinCF93 <Toojool@gmail.com>
2021-08-03 09:23:41 -0700

    Pokemon List
    Rewriting functions add and else functions to fit in addlistitem to create button list

--- js/script.js
+++ js/script.js
@@ -6,25 +6,38 @@ let pokemonRepository = (function () {
     ];
 
 
-    return {
-        add: function (pokemon1) {
-            if (pokemon1.name && pokemon1.types && pokemon1.weight){
-                pokemonList.push(pokemon1);
+    
+        function add(pokemonAdd) {
+            if (pokemonAdd.name && pokemonAdd.types && pokemonAdd.weight){
+                pokemonList.push(pokemonAdd);
             } else {
                 console.log('Not a Pokemon')
             }
-        },
-        getAll: function () {
+        }
+        function getAll() {
             return pokemonList;
         }
-    }
-}
-)();
+        function addListItem(pokemon) {
+            let ulList = document.querySelector('.pokemon_list');
+            let itemList = document.createElement('li');
+            let itemButton = document.createElement('button');
+            itemButton.innerText = pokemon.name;
+            itemButton.classList.add('my_button');
+            itemList.appendChild(itemButton);
+            ulList.appendChild(itemList);
+        }
+        return {
+            add:add,
+            getAll: getAll,
+            addListItem: addListItem,
+        }; 
+})();
 
-pokemonRepository.add({ name: 'Mewtwo', types: ['unkown'], weight: 0 });
+pokemonRepository.add({ name: 'Mewtwo', types: ['unkown'], weight: 40 });
+console.log(pokemonRepository.getAll());
+
 
 pokemonRepository.getAll().forEach(function (pokemon) {
-    document.write(pokemon.name + '(Weight:' + pokemon.weight + ')')
-    document.write("<br>");
+    pokemonRepository.addListItem(pokemon);
 }
 )
